a14aab15808789416954c3d9d862d5e55b726c0e
Will Eccles <ghostyx123@gmail.com>
2018-11-06 19:10:57 -0500

    added parallel_foreach to threading

--- src/threading.h
+++ src/threading.h
@@ -1,7 +1,10 @@
-#ifndef THREADING_H
-#define THREADING_H
+#ifndef WRESTD_THREADING_H
+#define WRESTD_THREADING_H
 
 #include <chrono>
+#include <thread>
+#include <vector>
+#include <cstdio>
 
 using namespace std::chrono;
 
@@ -9,12 +12,54 @@ namespace wrestd {
 	namespace threading {
 		template<class RType = double, class _Fn, class... _Args>
 		RType executionTime(_Fn&& fn, _Args&&... args) {
-			high_resolution_clock::time_point t1 = high_resolution_clock::now();
+			auto t1 = steady_clock::now();
 			fn(args...);
-			high_resolution_clock::time_point t2 = high_resolution_clock::now();
+			auto t2 = steady_clock::now();
 			duration<RType> timespan = duration_cast<duration<RType> >(t2-t1);
 			return (RType)timespan.count() * (RType)seconds::period::num/(RType)seconds::period::den;
-		}
+		};
+
+		// use multiple threads (up to max_threads or hardware_concurrency(), whichever is lower) to apply an operation
+		// to each element of a vector. the expression being run will accept two parameters:
+		//  - the element of the array
+		//  - the number of the thread (0-indexed)
+		// it will return the new value to be stored in that location in the vector.
+		template<typename iType>
+		void parallel_foreach(std::vector<iType> &v, std::size_t max_threads, iType (*op)(iType, std::size_t)) {
+			if (v.empty()) return; // if the user tries to perform the operation on an empty vector, return
+
+			// threadcount
+			std::size_t tcount;
+
+			// check if the user has put in more threads than they can handle, and if so, limit to hardware max.
+			if (max_threads > std::thread::hardware_concurrency()) max_threads = std::thread::hardware_concurrency();
+
+			// if the thread max is > than the length of the vector (in which case this is FAR less efficient than using one thread),
+			// use only the length worth of threads
+			if (max_threads > v.size()) tcount = v.size();
+			else if (max_threads <= v.size()) tcount = max_threads; // this is not exact, use mod later to fix this
+
+			std::vector<std::thread> threads(tcount);
+			std::size_t iterations_per_thread = v.size()/tcount;
+			
+			for (std::size_t t = 0; t < tcount; t++) { 
+				std::size_t f = t * iterations_per_thread; // index of first work item for this thread
+				std::size_t e = f + iterations_per_thread - 1; // index of last work item for this thread
+				
+				if (t == tcount - 1)
+					e += v.size() % tcount; // account for inaccuracies in division, if any
+
+				threads[t] = std::thread([=, &v, &op] {
+					for (std::size_t i = f; i <= e; i++) {
+						v[i] = op(v[i], t);
+					}
+				});
+			}
+
+			for (std::thread &t : threads) {
+				t.join();
+			}
+		};
 	};
 };
 
--- testing/testing.cpp
+++ testing/testing.cpp
@@ -1,14 +1,53 @@
 #include <iostream>
+#include <cstdio>
 #include <string>
-#include "stack.h"
+#include "threading.h"
+#include <vector>
+#include <chrono>
 
-using namespace wrestd::collections;
+using namespace wrestd::threading;
+using namespace std::chrono;
+
+void benchmark_singlethread(std::size_t datapoints) {
+	std::vector<int> vec(datapoints);
+	for (std::size_t i = 0; i < datapoints; i++) {
+		vec[i] = i%3;
+	}
+
+	std::cout << "Using " << datapoints << " pieces of sample data for single-threaded test...\n";
+
+	auto start = steady_clock::now();
+	for (std::size_t i = 0; i < datapoints; i++) {
+		vec[i] = (vec[i] * 2) % 4;
+	}
+	auto end = steady_clock::now();
+	std::printf("Finished single-thread benchmark in %.5g seconds.\n", duration_cast<duration<double>>(end - start).count() * (double)seconds::period::num / (double)seconds::period::den);
+}
+
+void benchmark_multithread(std::size_t datapoints) {
+	std::vector<int> vec(datapoints);
+	for (std::size_t i = 0; i < datapoints; i++)
+	{
+		vec[i] = i % 3;
+	}
+	std::size_t tcount = std::thread::hardware_concurrency();
+
+	std::cout << "Using " << datapoints << " pieces of sample data and " << tcount << " threads for a multi-threaded test...\n";
+
+	auto start = steady_clock::now();
+	parallel_foreach<int>(vec, tcount, [](int val, std::size_t t) -> int {
+		return (val * 2) % 4;
+	});
+	auto end = steady_clock::now();
+	std::printf("Finished multi-thread benchmark in %.5g seconds.\n", duration_cast<duration<double>>(end - start).count() * (double)seconds::period::num / (double)seconds::period::den);
+
+}
 
 int main(void) {
-	stack<int> s(12);
-
-	s.push(14);
-	std::cout << s.pop() << '\n' << s.pop() << '\n';
+	int count = 1'000'000'000;
+	std::cout << "This will take " << (sizeof(int) * count)/1000/1000 << "MB of memory, twice.\n";
+	benchmark_singlethread(count);
+	benchmark_multithread(count);
 
 	return 0;
 }
